4bb900b7f5f570f5b4f5e409480d28b9eeed0188
RaiyanJiyon <raiyanjiyon814@gmail.com>
2025-01-24 19:33:25 +0600

    Added post cards inside home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,42 @@
-export default function Home() {
+// page.js
+'use client';
+import Link from 'next/link';
+import React, { useEffect, useState } from 'react';
+
+const HomePage = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+        const data = await response.json();
+        setPosts(data);
+      } catch (error) {
+        console.error('Error fetching data:', error.message);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <div className="">
-      
+    <div>
+      <h1>Blogs Page</h1>
+      <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4'>
+        {posts.map((post) => (
+          <div key={post.id} class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white h-32">{post.title}</h5>
+            <Link href={`/blog/${post.id}`}>
+              <button className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 mr-4'>View Details</button>
+            </Link>
+          </div>
+
+        ))}
+      </div>
     </div>
   );
-}
+};
+
+export default HomePage;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
     ];
 
     return (
-        <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
+        <nav className="bg-white dark:bg-gray-900 w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                 <a href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
                     <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />
@@ -30,7 +30,7 @@ const Navbar = () => {
                 <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
                     <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                         {links.map((link, idx) => (
-                            <Link key={idx} className={`${pathname === link.path ? 'text-blue-500 font-bold' : 'text-white font-bold'}`} href={link.path}>
+                            <Link key={idx} className={`${pathname === link.path ? 'text-blue-500 font-bold' : 'text-black font-bold'}`} href={link.path}>
                                 {link.name}
                             </Link>
                         ))}
